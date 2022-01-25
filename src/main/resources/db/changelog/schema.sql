3f61acac96c93a967c687d656ca5b87c317b04fc
AntonChernyavskiy <vtb4058290@region.vtb.ru>
2022-01-25 10:08:38 +0300

    fix sql schema and docker compose script

--- src/main/resources/db/changelog/schema.sql
+++ src/main/resources/db/changelog/schema.sql
@@ -2,12 +2,10 @@ DROP TABLE IF EXISTS users;
 CREATE TABLE users
 (
     id        serial  NOT NULL,
-    username  varchar NOT NULL,
-    firstName varchar NOT NULL,
-    lastName  varchar NOT NULL,
+    firstname varchar NOT NULL,
+    lastname  varchar NOT NULL,
     email     varchar NOT NULL,
     phone     varchar NOT NULL,
-    CONSTRAINT uq_user_username UNIQUE (username),
-    CONSTRAINT uq_user_email UNIQUE (email),
-    CONSTRAINT uq_user_phone UNIQUE (phone)
+    CONSTRAINT uq_user_firstname UNIQUE (firstname),
+    CONSTRAINT uq_user_lastname UNIQUE (lastname)
 );
