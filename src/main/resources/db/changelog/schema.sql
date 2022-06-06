abc1701c881dbcdfd47e8c1da338d5c774a03bbc
AntonChernyavskiy <vtb4058290@region.vtb.ru>
2022-06-06 13:01:16 +0300

    v2 add news db schema for msa final project

--- src/main/resources/db/changelog/schema.sql
+++ src/main/resources/db/changelog/schema.sql
@@ -7,3 +7,13 @@ CREATE TABLE IF NOT EXISTS users.users
     password text UNIQUE NOT NULL,
     email    text UNIQUE NOT NULL
 );
+
+DROP TABLE IF EXISTS news.news;
+CREATE SCHEMA IF NOT EXISTS news;
+CREATE TABLE IF NOT EXISTS news.news
+(
+    id        UUID PRIMARY KEY                NOT NULL,
+    provider  TEXT                            NOT NULL,
+    timestamp TIMESTAMP WITH TIME ZONE UNIQUE NOT NULL,
+    payload   TEXT UNIQUE                     NOT NULL
+);
